04db124ed181bb9af229ae455797d7ebbcfd0452
deepti697 <116783856+deepti697@users.noreply.github.com>
2022-12-23 11:11:04 +0530

    Quiz Application

---
+++ QuizApplication/questions.js
@@ -0,0 +1,59 @@
+var questions =[
+    {
+        num : 1,
+        question : "HTML is considered as ___ language ",
+        answer : "Markup Language",
+        options : [
+            "OOP Language",
+            "High Level Language",
+            "Programming Language",
+            "Markup Language"
+        ]
+    },
+    {
+num: 2,
+question: "What are the types of unordered or bulleted list in HTML?",
+answer: "disc, circle, square",
+options: [
+"disc, square, triangle",
+"polygon, triangle, circle",
+"disc, circle, square",
+"All of the above"
+]
+},
+{
+num: 3,
+question: "A program in HTML can be rendered and read by",
+answer: "Web browser",
+options: [
+"Web browser",
+"Server",
+"Interpreter",
+"None of the above"
+]
+},
+
+{
+num: 4,
+question: "The tags in HTML are ",
+answer: "case-insensitive",
+options: [
+"case-sensitive",
+"in upper case",
+"case-insensitive",
+"in lowercase"
+]
+},
+{
+num: 5,
+question: "Which of the following are the attributes of a tag?",
+answer: "Both (a) & (b)",
+options: [
+"method",
+"action",
+"Both (a) & (b)",
+"None of the above"
+]
+},
+
+];
